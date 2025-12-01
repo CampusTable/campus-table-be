4a10955f9d20d295e55132ba3dc54c1d7cc6d5e4
kjh0718 <ranbow3062961@gmail.com>
2025-12-01 17:08:24 +0900

    메뉴_crud_기능_추가 : feat : {errorcode 수정} https://github.com/CampusTable/campus-table-be/issues/7

--- src/main/java/com/campustable/be/global/exception/ErrorCode.java
+++ src/main/java/com/campustable/be/global/exception/ErrorCode.java
@@ -16,6 +16,8 @@ public enum ErrorCode {
 
   ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
 
+  INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력 값이 유효하지 않거나 형식이 잘못되었습니다."),
+
   // AUTH
 
   AUTH_FAILED(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호를 확인해 주세요."),
@@ -24,6 +26,13 @@ public enum ErrorCode {
 
   SCRAPING_STRUCTURE_CHANGED(HttpStatus.INTERNAL_SERVER_ERROR, "대상 웹사이트의 구조가 변경되어 데이터를 가져올 수 없습니다."),
 
+  ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED,"Access Token이 만료되었습니다. 재발급이 필요합니다."),
+
+  JWT_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 인증 정보입니다. 다시 로그인해 주세요."),
+
+  REFRESH_TOKEN_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 리프레시 토큰입니다. 다시 로그인해 주세요."),
+
+  REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "리프레시토큰이 만료되었습니다. 다시 로그인해 주세요."),
   // MENU
   MENU_ALREADY_EXISTS(HttpStatus.CONFLICT, "해당 식당에 이미 존재하는 메뉴입니다."),
 
@@ -41,11 +50,20 @@ public enum ErrorCode {
 
   CAFETERIA_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 식당 코드입니다."),
 
+  OPERATING_HOURS_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 운영시간 정보를 찾을 수 없습니다."),
+
+  OPERATING_HOURS_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 운영시간입니다."),
+
   //Category
 
   CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 카테고리를 찾을 수 없습니다."),
 
-  CATEGORY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 카테고리입니다.");
+  CATEGORY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 카테고리입니다."),
+
+  //User
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+
+  USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 유저입니다.");
 
 
   private final HttpStatus status;
