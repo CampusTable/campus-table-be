ca3e88a4319556e096d18baadaed85372ee1e80b
seojeong <lsj2sj03@gmail.com>
2025-12-31 13:56:41 +0900

    CartItemDto에 MenuId추가

--- src/main/java/com/campustable/be/domain/cart/dto/CartItemDto.java
+++ src/main/java/com/campustable/be/domain/cart/dto/CartItemDto.java
@@ -13,6 +13,7 @@ public class CartItemDto {
   private int price;
   private String menuUrl;
   private Long cartItemId;
+  private Long menuId;
 
   public CartItemDto(CartItem entity) {
     this.menuName = entity.getMenu().getMenuName();
@@ -20,5 +21,6 @@ public class CartItemDto {
     this.quantity = entity.getQuantity();
     this.menuUrl =  entity.getMenu().getMenuUrl();
     this.cartItemId = entity.getCartItemId();
+    this.menuId = entity.getMenu().getId();
   }
 }
